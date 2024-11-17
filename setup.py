7bfba5e35a661b604dd6a51758ce7a05b6617bcb
Matthias <matthias.gaste42@gmail.com>
2024-11-17 04:41:31 +0100

    fs

--- setup.py
+++ setup.py
@@ -1,19 +1,14 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
-    name='LootBox',
+    name='PackageLootBox',
     version='0.0.1',
-    description=(
-        'A simple package for managing lootboxes with Create, Update, and Delete modules'
-    ),
-    long_description=long_description,
+    description='A package for managing lootboxes',
+    long_description='A package for managing lootboxes with Create, Update, and Delete modules.',
     long_description_content_type='text/markdown',
     author='MATIFIREofficiel',
     author_email='matthias.gaste42@gmail.com',
-    url='https://github.com/VyOk9/LootBox',
+    url='https://github.com/MATIFIREofficiel/PackageLootBox',
     packages=find_packages(),
     python_requires='>=3.12.0',
     include_package_data=True,
